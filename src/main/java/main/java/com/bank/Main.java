3c071c338d42dd520c6085da30a5e06dcd032e4d
nbepanam99 <nbepanam@gmail.com>
2022-12-21 16:23:34 +0100

    format code

--- src/main/java/main/java/com/bank/Main.java
+++ src/main/java/main/java/com/bank/Main.java
@@ -1,18 +1,23 @@
 package main.java.com.bank;
 
 import main.java.com.bank.domain.BankAccount;
+import main.java.com.bank.domain.Operation;
 
 import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     private static BankAccount bankAccount;
+    private static final DateTimeFormatter formatters=DateTimeFormatter.ofPattern("d/MM/uuuu HH:mm");
 
     public static void main(String[] args) {
+
         bankAccount = new BankAccount();
         Scanner scanner = new Scanner(System.in);
-        int selection=0;
+        int selection;
         var msg = """
                 ---------------------------------
                    -- 1 Deposit
@@ -25,24 +30,27 @@ public class Main {
             System.out.println(msg);
             selection = scanner.nextInt();
             switch (selection) {
-                case 1 : {
+                case 1 -> {
                     System.out.println("Please enter amount");
                     double amount = scanner.nextDouble();
-                    depositOperation(amount);
-                    break;
+                    try {
+                        depositOperation(amount);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                 }
-                case 2 : {
+                case 2 -> {
                     System.out.println("Please enter amount");
                     double amount = scanner.nextDouble();
-                    withdrawalOperation(amount);
-                    break;
+                    try {
+                        withdrawalOperation(amount);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+
                 }
-                case 3  :{
-                    historyOperation();
-                    break;
-                }
-                default:
-                    System.out.println("Quit");
+                case 3 -> historyOperation();
+                default -> System.out.println("Quit");
             }
 
         } while (selection < 4);
@@ -52,14 +60,19 @@ public class Main {
 
     private static void historyOperation() {
         System.out.println("History : ");
-        bankAccount.getHistory().forEach(operation -> System.out.println(
-                " Date : "+ operation.getDate() + " : " +operation.getType() + ", amount : " + operation.getAmount() + ", balance: " + operation.getBalance()
+        List<Operation> history = bankAccount.getHistory();
+        history.forEach(operation -> System.out.println(
+                " Date : "+ operation.getDate().format(formatters) + " : " + String.format("%1$"+10+ "s", operation.getType())
+                        + ", amount : " + operation.getAmount()
         ));
+
+        System.out.println("Balance : "+ history.get(history.size()-1).getBalance() +"€") ;
+
+
     }
 
     private static void withdrawalOperation(Double amount) {
         bankAccount.withdraw(new BigDecimal(amount));
-        System.out.println("Result : ");
     }
 
     private static void depositOperation(Double amount) {
